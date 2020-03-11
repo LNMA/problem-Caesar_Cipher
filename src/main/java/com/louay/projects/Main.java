81f5508ca31fc224fa8576f244056b6ae9588a2b
Louay Amr <louay_amr@outlook.com>
2020-03-11 03:05:52 +0200

    fix bug.

--- src/main/java/com/louay/projects/Main.java
+++ src/main/java/com/louay/projects/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input  = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("please input unencrypted massage: ");
         StringBuilder s = new StringBuilder(input.nextLine());
 
@@ -14,33 +14,33 @@ public class Main {
         StringBuilder lowerCase = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
         StringBuilder tempLowerCase = new StringBuilder();
         tempLowerCase.append(lowerCase.substring(rotate));
-        tempLowerCase.append(lowerCase.substring(0,rotate));
+        tempLowerCase.append(lowerCase.substring(0, rotate));
 
         StringBuilder upperCase = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         StringBuilder tempUpperCase = new StringBuilder();
         tempUpperCase.append(upperCase.substring(rotate));
-        tempUpperCase.append(upperCase.substring(0,rotate));
+        tempUpperCase.append(upperCase.substring(0, rotate));
 
-        System.out.println(tempLowerCase+ "  "+tempUpperCase);
-        String temp;
+        char temp;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) > 98 || s.charAt(i) <123){
+            temp = s.charAt(i);
+            if (temp > 98 || temp < 123) {
                 for (int j = 0; j < lowerCase.length(); j++) {
-                    if (s.charAt(i) == lowerCase.charAt(j)){
-                        s.setCharAt(i ,tempLowerCase.charAt(j));
+                    if (temp == lowerCase.charAt(j)) {
+                        s.setCharAt(i, tempLowerCase.charAt(j));
                     }
                 }
-            }else if (s.charAt(i) > 64 || s.charAt(i) < 91){
+            }
+            if (temp > 64 || temp < 91) {
                 for (int j = 0; j < upperCase.length(); j++) {
-                    if (s.charAt(i) == upperCase.charAt(j)){
-                        s.setCharAt(i ,tempUpperCase.charAt(j));
+                    if (temp == upperCase.charAt(j)) {
+                        s.setCharAt(i, tempUpperCase.charAt(j));
                     }
                 }
             }
         }
-        System.out.println();
-        System.out.println(s);
 
+        System.out.println("\n"+s);
 
     }
 }
